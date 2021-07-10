dfc0a7d547a7e9a3cb3a991d43b70fe5c4ac7001
AI Kiwi <66819523+Ai-Kiwi@users.noreply.github.com>
2021-07-10 14:36:13 +1200

    added my package manager to the list

--- dns/domains.py
+++ dns/domains.py
@@ -35,4 +35,5 @@ domains: Dict[str, Domain] = {
     "skydocs": { "cname": "skythecodemaster.github.io" },
     "thox": { "cname": "thox.touhey.pro" },
     "www": { "cname": "madefor.cc" },
+    "CloudGet": { "cname": "https://raw.githubusercontent.com/Ai-Kiwi/CC-CloudGet/main/Core.lua" }
 }
