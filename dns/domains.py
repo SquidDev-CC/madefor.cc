8f2ea437a8fa9a20aa8b5271e140d5c9a4d47f4d
Josh <dev@cldv.org>
2022-09-12 00:28:12 +0200

    add mc-cc-scripts.github.io

--- dns/domains.py
+++ dns/domains.py
@@ -34,6 +34,7 @@ domains: Dict[str, Domain] = {
     "plethora": { "cname": "squiddev-cc.github.io" },
     "potatos": { "cname": "osmarks.net" },
     "recrafted": { "cname": "ocawesome101.github.io" },
+    "scm": { "cname": "mc-cc-scripts.github.io" },
     "ships": {"cname":"ships.knijn.one"},
     "skydocs": { "cname": "skythecodemaster.github.io" },
     "skygui": { "cname": "skythecodemaster.github.io" },
