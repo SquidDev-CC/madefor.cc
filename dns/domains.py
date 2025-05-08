4ac18861510614cf7eb6bcde15343d0bd6ac66c1
Astronand <168994369+ASTRONAND@users.noreply.github.com>
2025-05-08 16:07:02 -0400

    Update domains.py

--- dns/domains.py
+++ dns/domains.py
@@ -51,6 +51,7 @@ domains: Dict[str, Domain] = {
     "siredvin": {"cname": "docs-redir.siredvin.site"},
     "skydocs": { "cname": "skythecodemaster.github.io" },
     "skygui": { "cname": "skythecodemaster.github.io" },
+    "Starlight-OS": { "cname": "starlight-cc.github.io" },
     "thox": { "cname": "thox.touhey.pro" },
     "turtlematic": {"cname": "docs-redir.siredvin.site"},
     "unicornpkg": { "cname": "unicornpkg.github.io" },
