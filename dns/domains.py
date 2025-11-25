e436df982df964c02cf39775bb3ef569d33b78b8
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2025-11-25 14:06:49 +0100

    Update domains.py (#84)
    Added CloverOS Wiki site to registry

--- dns/domains.py
+++ dns/domains.py
@@ -25,6 +25,7 @@ domains: Dict[str, Domain] = {
     "ccml": { "cname": "selim042.github.io" },
     "charmap": {"cname": "shrekshellraiser.github.io"},
     "cloveros": { "cname": "palordersoftworksofficial.github.io" },
+    "cloveros-wiki": { "cname": "palordersoftworksofficial.github.io" },
     "consult" : {"cname": "1Turtle.github.io"},
     "craftos-pc": { "cname": "admiring-shannon-be238c.netlify.app" },
     "datapacks": { "cname": "cc-tweaked.github.io" },
