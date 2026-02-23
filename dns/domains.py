bf8bbacebf3251f775ce4bc50aa87e81653f41c0
Aybri <61766021+aybri@users.noreply.github.com>
2026-02-23 14:44:30 -0600

    Update domains.py to remove impulsive-cc.madefor.cc
    impulsive-cc.madefor.cc is unused, and is an artifact of an older online identity. I'd like to remove it. 
    
    Also, when I originally added it, it was in the wrong order anyway.

--- dns/domains.py
+++ dns/domains.py
@@ -31,7 +31,6 @@ domains: Dict[str, Domain] = {
     "datapacks": { "cname": "cc-tweaked.github.io" },
     "epoch": { "cname": "haeleon.github.io" },
     "guih": { "cname": "9551-dev.github.io" },
-    "impulsive-cc": { "cname": "emeraldimpulse7.github.io" },
     "infernity": { "cname": "infernostars.github.io" },
     "kristify": { "cname": "kristify.github.io"},
     "metis": { "cname": "squiddev-cc.github.io" },
